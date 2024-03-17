7eac699f1b400b80903fed27a7f09b497a07bf7f
DaniilRuchkin <d.ruchkin@sh.dunice.net>
2024-03-17 19:12:17 +0300

    MainMenu

--- MainMenu.cs
+++ MainMenu.cs
@@ -1,41 +1,85 @@
 
 class MainMenu
     {
+        private static AddMenu addMenu = new AddMenu();
         public static void ShowMainMenu()
         {
+            string[] menuOptions =
+            {
+                "1. Сделать кофе",
+                "2. Добавить ингредиенты",
+                "3. Проверить ингредиенты",
+                "4. Проверить / очистить кофемашинку",
+                "5. Посмотреть историю готовки",
+                "8. Exit"
+            };
             while (true)
             {
-                Console.WriteLine("1. Сделать коффе");
-                Console.WriteLine("2. Добавить ингредиенты");
-                Console.WriteLine("3. Проверить ингредиенты");
-                Console.WriteLine("4. Проверить / очистить коффемашинку");
-                Console.WriteLine("8. Exit");
-                int result =  int.Parse(Console.ReadLine());
+                
+                Console.WriteLine("Меню:");
+                foreach (string option in menuOptions)
+                {
+                    Console.WriteLine(option);
+                }
+
+                int result = GetUserChoice();
                 switch (result)
                 {
                     case 1:
+                        Console.Clear();
                         CheckAndClear.CheckDirty();
                         Coffe.ShowCoffeMenu();
                         break;
                     case 2:
-                        AddMenu addMenu = new AddMenu();
                         Console.Clear();
                         addMenu.ShowMenu();
                         addMenu.ShowIngregient();
                         break;
                     case 3:
-                        addMenu = new AddMenu();
+                        Console.Clear();
                         addMenu.ShowIngregient();
                         break;
                     case 4:
+                        Console.Clear();
                         CheckAndClear.ShowDirty();
+                    break;
+                    case 5:
+                        ViewCreatingCoffe.ViewHistoryCoking();
                     break;
                     case 8:
                         Environment.Exit(0);
                     break;
                     default:
-                        Console.WriteLine("Unput incorect");
-                        break;
+                        Console.Clear();
+                        Console.WriteLine("Некоректное число");
+                    break;
+                }
+            }
+        }
+        static public int GetUserChoice()
+        {
+            Console.WriteLine("Введите ваш выбор: ");
+            while (true)
+            {
+                try
+                {
+                    int result = int.Parse(Console.ReadLine());
+                    if (result < 0)
+                    {
+                        Console.WriteLine("Ошибка: Введите положительное целое число.");
+                        Console.Write("Повторите попытку: ");
+                        continue;
+                    }
+                    return result;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: Некорректный формат ввода. Введите целое число.");
+                    Console.Write("Повторите попытку: ");
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Некоректное число");Console.Write("Повторите попытку: ");
                 }
             }
         }
